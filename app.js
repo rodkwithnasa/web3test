214e89308653d2c0ae1ea78a32df53da99b7f969
rodkwithnasa <atkins.howard@gmail.com>
2018-10-08 21:56:17 +0000

    Sets up initial ether passing

---
+++ app.js
@@ -0,0 +1,50 @@
+const Tx = require('ethereumjs-tx')
+const Web3 = require('web3')
+const web3 = new Web3('https://ropsten.infura.io/v3/ababe5982ca4452da54adcc689e42d04')
+
+// console.log(web3.eth.accounts.create())
+
+const acct1 = '0xC017001dD04Bee0756cb64DEE5D3dAc298EdDFCD'
+const acct2 = '0xaD7e7Bcd70941569185c7064F2C0b5AEeA5acd74'
+
+const pk1 = Buffer.from(process.env.PK1,'hex')
+//console.log('PK1=',process.env.PK1);
+//console.log('pk1=',pk1);
+
+const pk2 = Buffer.from(process.env.PK2,'hex')
+//console.log('PK2=',process.env.PK2);
+//console.log('pk2=',pk2);
+
+
+web3.eth.getBalance(acct1,(err,bal)=>{
+	console.log('acct1 bal:',web3.utils.fromWei(bal,'ether'))
+})
+
+web3.eth.getBalance(acct2,(err,bal)=>{
+	console.log('acct2 bal:',web3.utils.fromWei(bal,'ether'))
+})
+
+
+web3.eth.getTransactionCount(acct2, (err,txCount)=> {
+	// build
+	const txObject = {
+		nonce: web3.utils.toHex(txCount),
+		to: acct1,
+		value: web3.utils.toHex(web3.utils.toWei('1','ether')),
+		gasLimit: web3.utils.toHex(21000),
+		gasPrice: web3.utils.toHex(web3.utils.toWei('10','gwei'))
+	}
+	//console.log(txObject)
+	// sign
+	//console.log('pk2=',pk2)
+	const tx = new Tx(txObject)
+	tx.sign(pk2)
+	//console.log('tx=',tx)
+	const serializedTx = tx.serialize()
+	const raw = '0x' + serializedTx.toString('hex')
+	// broadcast
+	web3.eth.sendSignedTransaction(raw, (err,txHash)=> {
+		//console.log('txHash',txHash)
+		console.log('err, txHash=',err, txHash)
+	})
+})
